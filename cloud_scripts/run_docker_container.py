d4a77830830b604661e357aa832c3865e5bacc7d
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2021-08-05 16:35:55 -0400

    added environment file to load docker python sdk tools added script run_docker_container.py to launch mdtf-diagnostics docker container on cloud

---
+++ cloud_scripts/run_docker_container.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" run the MDTF-diagnostics docker container on the parallelWorks platform """
+import os
+import docker
+
+USER = "Jessica.Liptak@"
+MDTF_ROOT = os.path.join(USER,"pw","storage","mdtf")
+volume_list = {os.path.join(MDTF_ROOT,"inputdata"): {'bind': '/proj/inputdata', 'mode': 'ro'}, \
+   os.path.join(MDTF_ROOT,"wkdir"): {'bind': '/proj/wkdir', 'mode': 'rw'}, \
+   os.path.join(MDTF_ROOT,"MDTF-diagnostics/diagnostics"): {'bind': '/proj/MDTF-diagnostics/diagnostics', 'mode': 'ro'}, \
+   os.path.join(MDTF_ROOT,"MDTF-diagnostics/src"): {'bind': '/proj/MDTF-diagnostics/src', 'mode': 'ro'}, \
+   os.path.join(MDTF_ROOT,"MDTF-diagnostics/mdtf_framework.py"): {'bind': '/proj/MDTF-diagnostics/mdtf_framework.py', 'mode': 'ro'}, \
+   os.path.join(MDTF_ROOT,"MDTF-diagnostics/mdtf"): {'bind': '/proj/MDTF-diagnostics/mdtf', 'mode': 'ro'}, \
+   }
+
+client = docker.from_env()
+mdtf_container = client.containers.run('wrongkindofdoctor/mdtf.alpha01:latest', \
+   'mdtf -f ' + MDTF_ROOT + '/MDTF-diagnostics/src.default_tests.jsonc -v', \
+    volumes=volume_list, \
+    detach=True, \
+    entrypoint="/proj/MDTF-diagnostics/mdtf")
+
+
+print(mdtf_container.logs())
