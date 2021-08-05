6ec096edc9c77505247bc987753eadb0f2154815
Parallel Works User Account <Jessica.Liptak@noaa-user-1.us-east1-c.c.parallel-cluster-293819.internal>
2021-08-05 21:19:14 +0000

    fixed typos in path names and definitions

--- cloud_scripts/run_docker_container.py
+++ cloud_scripts/run_docker_container.py
@@ -4,7 +4,8 @@ import os
 import docker
 
 USER = "Jessica.Liptak@"
-MDTF_ROOT = os.path.join(USER,"pw","storage","mdtf")
+MDTF_ROOT = os.path.join("/pw","storage","mdtf")
+print(MDTF_ROOT)
 volume_list = {os.path.join(MDTF_ROOT,"inputdata"): {'bind': '/proj/inputdata', 'mode': 'ro'}, \
    os.path.join(MDTF_ROOT,"wkdir"): {'bind': '/proj/wkdir', 'mode': 'rw'}, \
    os.path.join(MDTF_ROOT,"MDTF-diagnostics/diagnostics"): {'bind': '/proj/MDTF-diagnostics/diagnostics', 'mode': 'ro'}, \
@@ -13,12 +14,15 @@ volume_list = {os.path.join(MDTF_ROOT,"inputdata"): {'bind': '/proj/inputdata', 
    os.path.join(MDTF_ROOT,"MDTF-diagnostics/mdtf"): {'bind': '/proj/MDTF-diagnostics/mdtf', 'mode': 'ro'}, \
    }
 
+
+
 client = docker.from_env()
-mdtf_container = client.containers.run('wrongkindofdoctor/mdtf.alpha01:latest', \
-   'mdtf -f ' + MDTF_ROOT + '/MDTF-diagnostics/src.default_tests.jsonc -v', \
+
+mdtf_container = client.containers.run('wrongkindofdoctor/mdtf.alpha-01:latest', \
+   'mdtf -f ' + MDTF_ROOT + '/MDTF-diagnostics/src/default_tests.jsonc -v', \
     volumes=volume_list, \
     detach=True, \
     entrypoint="/proj/MDTF-diagnostics/mdtf")
 
 
-print(mdtf_container.logs())
+#print(mdtf_container.logs())
