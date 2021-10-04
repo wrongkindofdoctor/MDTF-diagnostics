a49dd3f355d975bb0b43c64df6838fc44c09a8c5
wrongkindofdoctor <wrongkindofdoctor@noreply.github.com>
2021-10-04 20:41:45 +0000

    changed mount defs to use bind path in singularity.conf in run_mdtf_singularity_container.sh

--- cloud_scripts/run_mdtf_singularity_container.sh
+++ cloud_scripts/run_mdtf_singularity_container.sh
@@ -6,22 +6,25 @@
 #SBATCH --output=/contrib/${USER}/mdtf/CI_logs  # Standard output and error log
 
 # install required software
-sudo yum install -y debootstrap.noarch
+#sudo yum install -y debootstrap.noarch
 
 # Allows fakeroot:
-sudo sh -c 'echo user.max_user_namespaces=15000 \
-    >/etc/sysctl.d/90-max_net_namespaces.conf'
+#sudo sh -c 'echo user.max_user_namespaces=15000 \
+#    >/etc/sysctl.d/90-max_net_namespaces.conf'
 
-sudo sysctl -p /etc/sysctl.d /etc/sysctl.d/90-max_net_namespaces.conf
+#sudo sysctl -p /etc/sysctl.d /etc/sysctl.d/90-max_net_namespaces.conf
 
-sudo singularity config fakeroot --add ${USER}
+#sudo singularity config fakeroot --add ${USER}
 
 export MDTF_ROOT=/contrib/${USER}/mdtf
 # will probably need to modify /etc/singularity/singularity.conf BIND_PATH section instead to get this to work
-export SINGULARITY_BINDPATH="${MDTF_ROOT}/inputdata:/proj/mdtf/inputdata, \
-                        ${MDTF_ROOT}/wkdir:/proj/mdtf/wkdir, \
-                        ${MDTF_ROOT}/MDTF-diagnostics/diagnostics:/proj/mdtf/MDTF-diagnostics/diagnostics, \
-                        ${MDTF_ROOT}/MDTF-diagnostics/src/default_tests.jsonc:/proj/mdtf/MDTF-diagnostics/src/default_tests.jsonc "
+#export SINGULARITY_BINDPATH="${MDTF_ROOT}/inputdata:/proj/mdtf/inputdata, \
+#                        ${MDTF_ROOT}/wkdir:/proj/mdtf/wkdir, \
+#                        ${MDTF_ROOT}/MDTF-diagnostics/diagnostics:/proj/mdtf/MDTF-diagnostics/diagnostics, \
+#                        ${MDTF_ROOT}/MDTF-diagnostics/src/default_tests.jsonc:/proj/mdtf/MDTF-diagnostics/src/default_tests.jsonc "
+
+sudo sed -i 's|\#bind path = /scratch|bind path = /contrib/Jessica.Liptak/mdtf/inputdata:/proj/mdtf/inputdata\nbind path = /contrib/Jessica.Liptak/mdtf/wkdir:/proj/mdtf/wkdir\nbind path = /contrib/Jessica.Liptak/mdtf/MDTF-diagnostics/diagnostics:/proj/mdtf/MDTF-diagnostics/diagnostics\nbind path = /contrib/Jessica.Liptak/mdtf/MDTF-diagnostics/src/default_tests.jsonc:/proj/mdtf/MDTF-diagnostics/src/default_tests.jsonc |g' /etc/singularity/singularity.conf
+
 cd ${MDTF_ROOT}/MDTF-diagnostics
 
 sudo singularity run mdtf.sif -f /proj/mdtf/MDTF-diagnostics/src/default_tests.jsonc -v
