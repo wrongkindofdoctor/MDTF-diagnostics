eb36bb2ba301f042288c5e769fd4f30844403c7e
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2021-11-08 14:08:59 +0000

    added test jsonc for gfdl cm4 renamed test jsonc for cesm PODs and uncommented MJO PODs added lines to mount and run cm4 PODs

--- cloud_scripts/run_mdtf_singularity_container.sh
+++ cloud_scripts/run_mdtf_singularity_container.sh
@@ -12,8 +12,9 @@ if [ -d "wkdir" ]; then
 fi
 mkdir wkdir
 # define the bind paths in the singularity.conf file
-sudo sed -i 's|\#bind path = /scratch|bind path = /contrib/Jessica.Liptak/mdtf/inputdata:/proj/mdtf/inputdata\nbind path = /contrib/Jessica.Liptak/mdtf/wkdir:/proj/mdtf/wkdir\nbind path = /contrib/Jessica.Liptak/mdtf/MDTF-diagnostics/diagnostics:/proj/mdtf/MDTF-diagnostics/diagnostics\nbind path = /contrib/Jessica.Liptak/mdtf/MDTF-diagnostics/tests/pw_gcp_test_set1.jsonc:/proj/mdtf/MDTF-diagnostics/src/default_tests.jsonc |g' \
-   /etc/singularity/singularity.conf
+sudo sed -i 's|\#bind path = /scratch|bind path = /contrib/Jessica.Liptak/mdtf/inputdata:/proj/mdtf/inputdata\nbind path = /contrib/Jessica.Liptak/mdtf/wkdir:/proj/mdtf/wkdir\nbind path = /contrib/Jessica.Liptak/mdtf/MDTF-diagnostics/diagnostics:/proj/mdtf/MDTF-diagnostics/diagnostics\nbind path = /contrib/Jessica.Liptak/mdtf/MDTF-diagnostics/tests/github_actions_test_pw_gcp_qboi_cesm.jsonc:/proj/mdtf/MDTF-diagnostics/tests/github_actions_test_pw_gcp_qboi_cesm.jsonc
+  \nbind path = /contrib/Jessica.Liptak/mdtf/wkdir:/proj/mdtf/wkdir\nbind path = /contrib/Jessica.Liptak/mdtf/MDTF-diagnostics/diagnostics:/proj/mdtf/MDTF-diagnostics/diagnostics\nbind path = /contrib/Jessica.Liptak/mdtf/MDTF-diagnostics/tests/github_actions_test_pw_gcp_gfdl_cm4.jsonc:/proj/mdtf/MDTF-diagnostics/tests/github_actions_test_pw_gcp_gfdl_cm4.jsonc |g' \
+  /etc/singularity/singularity.conf
 
 # clone the MDTF-diagnostics repo
 cd ${MDTF_ROOT}
@@ -24,4 +25,5 @@ fi
 git clone -b add_docker_image https://github.com/wrongkindofdoctor/MDTF-diagnostics.git
 
 # run the singularity container
-sudo singularity run mdtf_diagnostics.sif -f /proj/mdtf/MDTF-diagnostics/src/default_tests.jsonc -v
+sudo singularity run mdtf_diagnostics.sif -f /proj/mdtf/MDTF-diagnostics/tests/github_actions_test_pw_gcp_qboi_cesm.jsonc -v
+sudo singularity run mdtf_diagnostics.sif -f /proj/mdtf/MDTF-diagnostics/tests/github_actions_test_pw_gcp_gfdl_cm4.jsonc -v
