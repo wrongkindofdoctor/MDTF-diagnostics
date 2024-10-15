c8e7764ac0935581adbc4c7db567945515846eb4
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2024-10-15 12:35:50 -0400

    add zarr to conda envs add test_zarr file to src unit tests±

---
+++ src/tests/test_zarr.py
@@ -0,0 +1,11 @@
+import numpy as np
+import xarray as xr
+
+def generate_data():
+    t = np.random.uniform(low=208.0, high=312.0, size=(365, 144, 96, 10))
+    time = range(15,366,30)
+def write_zarr():
+    pass
+
+def read_zarr():
+    pass
