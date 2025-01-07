1d158cd0706b06f704c53f71d83f123ffdcf3f86
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2025-01-07 12:34:47 -0500

    define click options for generate_file_metadata

--- tools/generate_file_metdata/generate_file_metadata.py
+++ tools/generate_file_metdata/generate_file_metadata.py
@@ -13,6 +13,8 @@
 import click
 import kerchunk
 import fsspec
+import os
+import sys
 import ujson
 
 
@@ -27,8 +29,38 @@ def gen_json_metadata_files(file_path: str, output_dir: str):
         with open(f"{output_dir}/{file_path.split('/')[-1]}.json", 'wb') as outf:
             outf.write(ujson.dumps(h5chunks.translate()).encode())
 
-def main():
-    pass
+@click.option('-i',
+              '--input_dir',
+              required=False,
+              default=lambda: os.getcwd(),
+              show_default = '(Current Working Directory)',
+              type=click.Path(),
+              help='Path to the directory with target files'
+              )
+@click.option("-o",
+              "--output_dir",
+              type=click.Path(),
+              required=False,
+              default=lambda: os.getcwd(),
+              show_default = '(Current Working Directory)',
+              help="Directory where metadata file will be written")
+@click.option("-s",
+              "--file_system",
+              type=click.Choice(['local', 'aws'], case_sensitive=False),
+              required=False,
+              default='local',
+              help="Type of system files are stored on")
+@click.command()
+def main(input_dir: click.Path, output_dir: click.Path, file_system) -> int :
+    config = dict({'input_dir': input_dir,
+                   'output_dir': output_dir,
+                   'system': file_system}
+                  )
+    for k, v in config.items():
+        print(f'Config {k} : {v}')
+
+
 
 if __name__ == '__main__':
-    main(prog_name='generate file metadata')
+    exit_code = main(prog_name='generate file metadata')
+    sys.exit(exit_code)
