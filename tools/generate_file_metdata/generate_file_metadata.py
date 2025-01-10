296918781926aad065b4054d4fcc79ddc00e1c6c
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2025-01-10 12:20:49 -0500

    refine error reporting add placehold routine to convert nc3 to nc4

--- tools/generate_file_metdata/generate_file_metadata.py
+++ tools/generate_file_metdata/generate_file_metadata.py
@@ -11,30 +11,35 @@
 # Output: json file with metadata for files in the input_dir
 
 import click
-import dask
+from kerchunk.combine import MultiZarrToZarr
 from kerchunk.hdf import SingleHdf5ToZarr
 import fsspec
 import glob
+import json
 import os
 import sys
-import json
-import uuid
-from dask.distributed import Client
+from tempfile import TemporaryDirectory
+
+def convert_netcdf(input_file, output_dir,old_version, new_version):
+    """Convert netcdf 3/classic to netcdf 4/hdf5"""
+    pass
 
 def write_fsspec(fs_read, input_file, output_dir):
     with fs_read.open(input_file, **dict(mode="rb")) as infile:
         print(f"Running kerchunk generation for {input_file}...")
         # Chunks smaller than `inline_threshold` will be stored directly in the reference file as data (as opposed to a URL and byte range).
+        h5chunks = SingleHdf5ToZarr(infile, input_file, inline_threshold=300)
         file_name = os.path.basename(input_file)
         file_name = file_name.replace('.nc', '.json')
         out_file_name = output_dir + '/' + file_name
         if os.path.isfile(out_file_name):
             print(f'Deleting existing file in {output_dir}')
             os.remove(out_file_name)
-        print(out_file_name)
-        h5chunks = SingleHdf5ToZarr(infile, input_file, inline_threshold=300)
+
         with open(out_file_name, "wb") as f:
             f.write(json.dumps(h5chunks.translate()).encode())
+        print(f"Finished writing {out_file_name}")
+        return out_file_name
 
 
 @click.option('-i',
@@ -73,7 +78,7 @@ def run(input_dir: click.Path, output_dir: click.Path, file_system) -> int :
     if not dir_path.endswith('/'):
         dir_path += '/'
     # anon=True if dataset on AWS does not require users to be logged
-    fs_read = fsspec.filesystem(config['system'], anon=True)
+    fs_read = fsspec.filesystem(config['system'])
     if config['system'] == 'local':
         dir_path += '**/*.nc'
         file_paths = glob.glob(dir_path, recursive=True)
@@ -86,10 +91,25 @@ def run(input_dir: click.Path, output_dir: click.Path, file_system) -> int :
     else:
         print(f"No files found in {dir_path}")
         return 1
-
-    out_dir = os.path.join(config['output_dir'], 'fsspec_refs')
-    os.makedirs(out_dir, exist_ok=True)
-    dask.compute(*[dask.delayed(write_fsspec)(fs_read, f, out_dir) for f in file_paths])
+    temp_dir = TemporaryDirectory(prefix=config['output_dir'])
+    assert os.path.isdir(temp_dir.name), "{out_dir} is not a directory".format(out_dir=temp_dir)
+    try:
+        output_files = [write_fsspec(fs_read, f, temp_dir.name) for f in file_paths]
+        # combine individual references into single consolidated reference
+        mzz = MultiZarrToZarr(
+            output_files,
+            remote_protocol=config['system'],
+            remote_options={'anon': True},
+            concat_dims=['time'],
+            coo_map={'time': 'cf:time'},
+            # inline_threshold=0 means don't story any raw data in the kerchunk reference file.
+            inline_threshold=0
+        )
+        multi_kerchunk = mzz.translate()
+    except Exception as exc:
+        print(exc)
+        temp_dir.cleanup()
+        return 1
     return 0
 
 if __name__ == '__main__':
